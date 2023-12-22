2d846a535f06986f78d9f9b51e97302d5b224dfc
ptak18 <ptakyie@gmail.com>
2023-12-22 19:44:41 +0000

    fixed bug in for loop

--- script.js
+++ script.js
@@ -7,21 +7,18 @@ $("#currentDay").text(today.format("D MMM YYYY"));
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 var currentHour = today.format("H");
 console.log(currentHour);
-for (let index = 9; index < array.length; index++) {
+for (let index = 9; index < 18; index++) {
   var currentDiv = $("#hour-" + index);
 
   if (index > currentHour) {
     currentDiv.addClass("future");
-    currentDiv.removeClass("past");
-    currentDiv.removeClass("present");
-  } else if ((index = currentHour)) {
+   
+  } else if ((index == currentHour)) {
     currentDiv.addClass("present");
-    currentDiv.removeClass("past");
-    currentDiv.removeClass("future");
+   
   } else if (index < currentHour) {
     currentDiv.addClass("past");
-    currentDiv.removeClass("future");
-    currentDiv.removeClass("present");
+   
   }
 }
 // Save the event in local storage when the save button is clicked in that timeblock.
