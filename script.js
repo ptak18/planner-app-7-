455a757d3d094b5dc712bd1767c48e9664054439
ptak18 <ptakyie@gmail.com>
2023-12-22 19:38:55 +0000

    created for loop to update classes

--- script.js
+++ script.js
@@ -4,13 +4,26 @@
 var today = dayjs();
 $("#currentDay").text(today.format("D MMM YYYY"));
 
- 
-
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+var currentHour = today.format("H");
+console.log(currentHour);
+for (let index = 9; index < array.length; index++) {
+  var currentDiv = $("#hour-" + index);
 
-// Allow a user to enter an event when they click a timeblock
-
+  if (index > currentHour) {
+    currentDiv.addClass("future");
+    currentDiv.removeClass("past");
+    currentDiv.removeClass("present");
+  } else if ((index = currentHour)) {
+    currentDiv.addClass("present");
+    currentDiv.removeClass("past");
+    currentDiv.removeClass("future");
+  } else if (index < currentHour) {
+    currentDiv.addClass("past");
+    currentDiv.removeClass("future");
+    currentDiv.removeClass("present");
+  }
+}
 // Save the event in local storage when the save button is clicked in that timeblock.
 
 // Persist events between refreshes of a page
-
